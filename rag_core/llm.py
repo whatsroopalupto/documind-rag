719922b7cf61769a8f3d84ff058b2c2e555af2d5
whatsroopalupto <roopal22sh@gmail.com>
2026-03-21 00:47:11 +0530

    Added llm.py for Gemini setup

---
+++ rag_core/llm.py
@@ -0,0 +1,13 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_llm():
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-flash",
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        temperature=0.3
+    )
+    return llm
